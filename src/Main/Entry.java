389da106092217018713945e9b111733e86b198c
Nuo (Tony) Chen <chen2886@purdue.edu>
2020-05-11 21:29:02 -0400

    Initial setup

---
+++ src/Main/Entry.java
@@ -0,0 +1,54 @@
+package Main;
+
+import java.time.LocalDate;
+
+public class Entry {
+
+    int serialNum;
+    String title;
+    String name;
+    String email;
+    String suggestions;
+    LocalDate checkInDate;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSuggestions() {
+        return suggestions;
+    }
+
+    public void setSuggestions(String suggestions) {
+        this.suggestions = suggestions;
+    }
+
+    public LocalDate getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(LocalDate checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+}
